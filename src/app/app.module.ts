a318e257e27c8fea1c9006a3067f47ab50cb2e31
AlaaSaadeddin <alaa.swaireh@gmail.com>
2021-06-06 23:46:14 +0300

    create product card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,16 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MaterialModule} from './material/material.module';
 import { SideMenuComponent } from './components/side-menu/side-menu.component';
-import { CategoryCardComponent } from './components/category-card/category-card.component'
+import { CategoryCardComponent } from './components/category-card/category-card.component';
+import { ProductCardComponent } from './components/product-card/product-card.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     SideMenuComponent,
-    CategoryCardComponent
+    CategoryCardComponent,
+    ProductCardComponent
   ],
   imports: [
     BrowserModule,
